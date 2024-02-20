b03c82ff785d39153881f12793717a126979d203
Kent Russell Casiño <cwrkent@gmail.com>
2024-02-20 14:30:37 +0800

    Fixed. Line 22

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     if ($result->num_rows > 0) {
         // output data of each row
         while ($row = $result->fetch_assoc()) {
-            echo $row['regDesc'] . "<br/>";
+            echo $row['regDesc'] . "<hr/>";
         }
     } else {
         echo "0 results";
