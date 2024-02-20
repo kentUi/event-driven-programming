c479eaf57a332d03a21715c6419c65e0dcfee586
Kent Russell Casiño <cwrkent@gmail.com>
2024-02-20 13:47:42 +0800

    Activity 1

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php 
+// Database Credentials
+$servername = "localhost";
+$username = "root";
+$password = ""; // Default Password: Blank | Empty
+
+$database_name = "db_api";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database_name);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//echo "Connected successfully";
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+    <?php
+    include('./config/database.php');
+
+    $sql = "SELECT * FROM ph_region WHERE regDesc LIKE 'region i%'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo $row['regDesc'] . "<br/>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
